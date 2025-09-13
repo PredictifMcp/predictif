46b68029b85e35305c2d8aba9929e882852851c2
dirdr <adrien.pelfresne@gmail.com>
2025-09-13 16:24:38 +0200

    feat() add echo function

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from mcp.server.fastmcp import FastMCP
 import pandas as pd
+from pydantic import Field
 
-mcp = FastMCP("Predictif MCP Server", port=3000, stateless_http=True, debug=True)
+mcp = FastMCP("Predictif MCP Server", port=3003, stateless_http=True, debug=True)
+
+
+@mcp.tool(
+    title="Echo Tool",
+    description="Echo the input text",
+)
+def echo(text: str = Field(description="The text to echo")) -> str:
+    return text
 
 
 @mcp.tool(
